6289dd369053097281bc8da9e50c9ba04d70a310
Abhay2003-ops <abhaymehra220@gmail.com>
2025-02-03 16:55:14 +0530

    FOLDER/FILE_Creation

---
+++ Devops_PathFinder/constants.py
@@ -0,0 +1,49 @@
+# constants.py
+
+# List of DevOps Tools
+DEVOPS_TOOLS = [
+    "Ansible",
+    "Docker",
+    "Kubernetes",
+    "Terraform",
+    "Jenkins",
+    "Git",
+    "AWS",
+    "Azure",
+    "GCP"
+]
+
+# Expertise Levels
+EXPERTISE_LEVELS = {
+    "beginner": "Beginner",
+    "intermediate": "Intermediate",
+    "expert": "Expert"
+}
+
+# Cloud Services
+CLOUD_SERVICES = [
+    "AWS",
+    "Azure",
+    "GCP"
+]
+
+# Certifications
+CERTIFICATIONS = [
+    "AWS Certified Solutions Architect",
+    "Certified Kubernetes Administrator (CKA)",
+    "Docker Certified Associate",
+    "HashiCorp Certified: Terraform Associate"
+]
+
+# Labels for Time Commitment
+TIME_COMMITMENT = {
+    "short_term": "1-2 hours per week",
+    "medium_term": "3-5 hours per week",
+    "long_term": "6+ hours per week"
+}
+
+# General Labels
+YES_NO = {
+    "yes": "Yes",
+    "no": "No"
+}
